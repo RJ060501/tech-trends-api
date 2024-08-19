b9ea38d2c1d64b6d38cbb9959a60941ee8248a0e
RJ060501 <rjrussell605@gmail.com>
2024-08-19 10:20:58 -0600

    implemented a keyword search

--- index.js
+++ index.js
@@ -1,81 +1,84 @@
-const PORT = process.env.PORT || 8000
-const express = require('express')
-const axios = require('axios')
-const cheerio = require('cheerio')
+const PORT = process.env.PORT || 8000;
+const express = require("express");
+const axios = require("axios");
+const cheerio = require("cheerio");
 
-const app = express()
+const app = express();
+
+// Define the keyword to filter articles by URL
+const keyword = ""; // Change this to your desired keyword
 
 const newspapers = [
-    {
-        name: 'yellowsystems',
-        address: 'https://yellow.systems/blog/web-development-future-trends',
-        base: ''
-    },
-    {
-        name: 'explodingtopics',
-        address: 'https://explodingtopics.com/blog/software-development-trends',
-        base: ''
-    }
-    //TODO: Add more newspapers to collect more data
-]
+  {
+    name: "yellowsystems",
+    address: "https://yellow.systems/blog/web-development-future-trends",
+    base: "",
+  }
+  // Additional newspapers can be added here
+];
 
-const articles = []
+const articles = [];
 
-newspapers.forEach(newspaper => {
-    axios.get(newspaper.address)
-        .then(response => {
-            const html = response.data
-            const $ = cheerio.load(html)
+newspapers.forEach((newspaper) => {
+  axios.get(newspaper.address).then((response) => {
+    const html = response.data;
+    const $ = cheerio.load(html);
 
-            $('a', html).each(function () {
-                if ($(this).attr('href').startsWith("https")) {
-                    const title = $(this).text()
-                    const url = $(this).attr('href')
-    
-                    articles.push({
-                        title,
-                        url: newspaper.base + url,
-                        source: newspaper.name
-                    })
-                }
-            })
-        })
-})
+    $("a", html).each(function () {
+      const url = $(this).attr("href");
 
-app.get('/', (req, res) => {
-    res.json('Welcome to my Tech Trends API')
-})
+      // Only include articles where the URL contains the keyword
+      if (url && url.startsWith("https") && url.includes(keyword)) {
+        const title = $(this).text();
+        articles.push({
+          title,
+          url: newspaper.base + url,
+          source: newspaper.name,
+        });
+      }
+    });
+  }).catch((err) => console.log(err));
+});
 
-app.get('/news', (req, res) => {
-    res.json(articles)
-})
+app.get("/", (req, res) => {
+  res.json("Welcome to my Tech Trends API");
+});
 
-app.get('/news/:newspaperID', (req, res) => {
-    const newspaperID = req.params.newspaperID
-    
-    const newspaperAddress = newspapers.filter(newspaper => newspaper.name == newspaperID)[0].address
-    const newspaperBase = newspapers.filter(newspaper => newspaper.name == newspaperID)[0].base
+app.get("/news", (req, res) => {
+  res.json(articles);
+});
 
-    axios.get(newspaperAddress)
-        .then(response => {
-            const html = response.data
-            const $ = cheerio.load(html)
-            const specificArticles = []
+app.get("/news/:newspaperID", (req, res) => {
+  const newspaperID = req.params.newspaperID;
 
-            $('a', html).each(function () {
-                if ($(this).attr('href').startsWith("https")) {
-                    const title = $(this).text()
-                    const url = $(this).attr('href')
-    
-                    specificArticles.push({
-                        title,
-                        url: newspaperBase + url,
-                        source: newspaperID
-                    })
-                }
-            })
-            res.json(specificArticles)
-        }).catch(err => console.log(err))
-})
+  const newspaper = newspapers.find(newspaper => newspaper.name === newspaperID);
+  if (newspaper) {
+    axios
+      .get(newspaper.address)
+      .then((response) => {
+        const html = response.data;
+        const $ = cheerio.load(html);
+        const specificArticles = [];
 
-app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
+        $("a", html).each(function () {
+          const url = $(this).attr("href");
+
+          // Only include articles where the URL contains the keyword
+          if (url && url.startsWith("https") && url.includes(keyword)) {
+            const title = $(this).text();
+            specificArticles.push({
+              title,
+              url: newspaper.base + url,
+              source: newspaperID,
+            });
+          }
+        });
+        res.json(specificArticles);
+      })
+      .catch((err) => console.log(err));
+  } else {
+    res.status(404).json({ message: "Newspaper not found" });
+  }
+});
+
+app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
